52dff08c1140af707cac794ed13a0fb7c4835e3c
Nicolas <souzanicolas87@gmail.com>
2021-05-12 19:52:10 -0300

    Commit inicial

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="src/www/styles/css/index.css">
+    <link rel="stylesheet" type="text/css" href="src/www/styles/css/global.css">
+    <title>Hora!</title>
+</head>
+<body>
+    <h1>Está na hora!</h1>
+</body>
+</html>
