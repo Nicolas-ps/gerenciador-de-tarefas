2b8a779cc128dd17af78c291089f919fd35dd0d7
Nicolas <souzanicolas87@gmail.com>
2021-05-13 21:47:32 -0300

    Estrutura inicial

---
+++ src/www/pages/home.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="../styles/css/global.css">
+    <link rel="stylesheet" type="text/css" href="../styles/home.scss">
+    <title>A Hora!</title>
+</head>
+<body>
+    <h1>It's Time!!!</h1>
+    <h2><a href="../../../index.php">Index</a></h2>
+    <h2><a href="cadastro.php">Cadastro</a></h2>
+</body>
+</html>
