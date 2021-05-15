115cb0c238fe97f946acd5e59912519db7d388fd
Nicolas <souzanicolas87@gmail.com>
2021-05-15 14:56:23 -0300

    formatação estilização concluída

--- src/www/pages/cadastro.php
+++ src/www/pages/cadastro.php
@@ -5,12 +5,60 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="../styles/css/global.css">
-    <link rel="stylesheet" type="text/css" href="../styles/css/cadastro.scss">
+    <link rel="stylesheet" type="text/css" href="../styles/css/index.css">
     <title>A Hora!</title>
 </head>
 <body>
-    <h1>It's Time!!!</h1>
-    <h2><a href="../../../index.php">Index</a></h2>
-    <h2><a href="home.php">Home</a></h2>
+<div class="containerPrincipal">
+
+<div class="containerApresentacao">
+    <div class="subcontainerApresentacao">
+        <div class="apresentacao">
+            <div class="titulo">
+                Seja bem vindo ao Hora!
+            </div>
+            <div class="textos">
+                <div class="texto">
+                    O Hora! é uma plataforma de gerenciamento de tarefas online simples e dinâmica. Aqui você faz seu controle de tarefas e pendências sem complicacões. Com alguns cliques, uma nota de texto e <i>voilà</i>, a tarefa foi adicionada à sua lista!
+                </div>
+                <div class="texto">
+                    O Hora! é uma plataforma de gerenciamento de tarefas online simples e dinâmica. Aqui você faz seu controle de tarefas e pendências sem complicacões. Com alguns cliques, uma nota de texto e <i>voilà</i>, a tarefa foi adicionada à sua lista!
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="containerLogin">
+    <div class="containerLogo">
+            <div class="logo">
+                <img src="../../../public/img/logo-hora-removebg.png" alt="Logo do Hora!">
+            </div>
+            <div class="logomarca">
+                <img src="../../../public/img/logo-texto-ssimbol-removebg.png">
+            </div>
+    </div>
+    <div class="containerFormulario">
+        <form method="POST" >
+
+            <div class="inputs">         
+                <input type="email" placeholder="Digite seu email">
+                <input type="password" placeholder="Digite sua senha">
+                <span><a href="#">Esqueceu sua senha?</a></span>
+            </div>
+
+            <div class="botao">
+                <button type="submit">Entrar</button>
+                <div class="span">
+                    <span>Não tem uma conta? <a href="src/www/pages/cadastro.php">Cadastre-se</a> </span>
+                </div>  
+            </div>
+
+
+        </form>
+    </div>
+</div>
+
+</div>
 </body>
 </html>
