0287ec084a05eb90d044f82eba438131f7e93705
Nicolas <souzanicolas87@gmail.com>
2021-05-15 11:22:28 -0300

    variáveis sass

--- index.php
+++ index.php
@@ -9,8 +9,49 @@
     <title>Hora!</title>
 </head>
 <body>
-    <h1>Está na hora!</h1> 
-    <h1><a href="src/www/pages/home.php">Home</a></h1>  
-    <h1><a href="src/www/pages/cadastro.php">Cadastro</a></h1>
+    <div class="containerPrincipal">
+
+        <div class="containerApresentacao">
+            <div class="apresentacao">
+                <div class="titulo">
+                    Seja Bem vindo ao Hora!
+                </div>
+                <div class="textos">
+                    <div class="textoUm">
+                        O Hora! é uma plataforma de gerenciamento de tarefas online simples e dinâmica. Aqui você faz seu controle de tarefas e pendências sem complicacões. Com alguns cliques, uma nota de texto e <i>voilà</i>, a tarefa foi adicionada à sua lista!
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="containerLogin">
+            <div class="containerLogo">
+                    <div class="logo">
+                        <img src="public/img/logo-hora-removebg.png" alt="Logo do Hora!" width="150px" height="100px">
+                    </div>
+                    <div class="logomarca">
+                        <img src="public/img/logo-texto-ssimbol-removebg.png" width="150px" height="100px">
+                    </div>
+            </div>
+            <div class="containerFormulario">
+                <form>
+
+                    <div class="inputs">         
+                        <input type="email" placeholder="Digite seu email">
+                        <input type="password" placeholder="Digite sua senha">
+                        <span><a href="#">Esqueceu sua senha?</a></span>
+                    </div>
+
+                    <div class="botao">
+                        <button type="submit">Entrar</button>
+                        <span>Não tem uma conta? <a href="src/www/pages/cadastro.php">Cadastre-se</a> </span>
+                    </div>
+
+
+                </form>
+            </div>
+        </div>
+
+    </div>
 </body>
 </html>
